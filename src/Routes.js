b092704c98f9b97cfb611e75b8340b4a691e45a3
SaurabhSonde <saurabhsonde111@gmail.com>
2021-07-11 20:49:54 +0530

    Routes added

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,25 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import PrivateRoute from "./auth/helper/PrivateRoute";
+import Signup from "./user/Signup";
+import Signin from "./user/Signin";
+import Todo from "./user/Todo";
+import CreateTodo from "./user/CreateTodo";
+import UpdateTodo from "./user/UpdateTodo";
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={App} />
+        <Route path="/" exact component={Signup} />
+        <Route path="/signin" exact component={Signin} />
+        <PrivateRoute path="/user/todo" exact component={Todo} />
+        <PrivateRoute path="/user/create/todo" exact component={CreateTodo} />
+        <PrivateRoute
+          path="/user/update/todo/:todoId/:userId"
+          exact
+          component={UpdateTodo}
+        />
       </Switch>
     </BrowserRouter>
   );
